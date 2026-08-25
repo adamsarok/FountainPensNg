17520bfd569c6785ad4c1bbbfb8d1fac4ff043a7
Ádám Sarok <sarok.adam@gmail.com>
2026-08-25 22:50:23 +0200

    fix

--- FountainPensNg.Server/Program.cs
+++ FountainPensNg.Server/Program.cs
@@ -58,7 +58,7 @@ using (var scope = app.Services.CreateScope()) {
 app.UseCors(x => x.AllowAnyHeader()
 	.AllowAnyMethod()
 	.AllowCredentials()
-	.WithOrigins("http://localhost", "http://192.168.1.79:4200", "https://192.168.1.79:4200", "https://localhost:4200", "http://localhost:4200", "http://localhost:8080"));
+	.WithOrigins("http://localhost", "http://192.168.1.67:4200", "https://192.168.1.67:4200", "https://localhost:4200", "http://localhost:4200", "http://localhost:8080"));
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
--- fountainpensng.client/src/environments/environment.ts
+++ fountainpensng.client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://192.168.1.79:4080',
+    apiUrl: 'http://192.168.1.67:4080',
 };
